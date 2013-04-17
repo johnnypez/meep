2bd4714fd171d253f91615a672d4905da7b44659
John Butler <johnnypez@gmail.com>
2013-04-17 23:24:01 +0100

    action tracking needs to change a little

--- spec/facebook_spec.rb
+++ spec/facebook_spec.rb
@@ -14,7 +14,7 @@ describe Meep::Facebook do
   end
 
   it "should track action referrals" do
-    res = Meep.fb_action_referral "thedoveapp:compliment", user: 'userA', action_id: "10151351033270737", object_id: '360985147344774'
+    res = Meep.fb_action_referral "thedoveapp:compliment", source: 'other_multiline', user: 'userA', action_id: "10151351033270737", object_id: '360985147344774'
   end
 
 end
